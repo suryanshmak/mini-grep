06c0612c6e178b6e89fe01d75ee7c6630abdcfbd
Suryansh Makharia <makhariasuryansh7@gmail.com>
2022-05-08 13:33:33 +0530

    print errors to standard error stream and change the output stream to output.txt

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ fn main() {
 
     let config = match Config::new(&args) {
         Err(err) => {
-            println!("Application error: {}", err);
+            eprintln!("Application error: {}", err);
             process::exit(1);
         }
         Ok(config) => config,
     };
 
     if let Err(err) = mini_grep::run(config) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
 
         process::exit(1);
     }
